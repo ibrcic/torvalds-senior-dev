5c11e24646a1a86e476c292efa4ac1058beb32ba
hjn5906 <hjn5906@rit.edu>
2017-02-11 12:54:32 -0500

    refactor: changes ItemCollection into a Map collection

--- android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/model/ItemCollection.java
+++ android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/model/ItemCollection.java
@@ -1,12 +1,13 @@
 package torvalds.istinventorymanagement.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Hassan Jegan Ndow on 1/29/2017.
  * Stores a collection of items
  */
 
-public class ItemCollection<Item> extends ArrayList<Item> {
+public class ItemCollection<String, Item> extends HashMap<String,Item> {
     private static final long serialVersionUID = 1L;
 }
