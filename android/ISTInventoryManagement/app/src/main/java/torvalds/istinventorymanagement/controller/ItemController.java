6f0adcf232453be06a1bd73e8e6eea05021e10b7
hjn5906 <hjn5906@rit.edu>
2017-02-23 22:43:27 -0500

    feat: adds Controller package

---
+++ android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/controller/ItemController.java
@@ -0,0 +1,9 @@
+package torvalds.istinventorymanagement.controller;
+
+/**
+ * Created by Hassan Jegan Ndow on 2/23/2017.
+ * Handles the I/O between the Model and Item View
+ */
+
+public class ItemController {
+}
---
+++ android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/controller/MasterController.java
@@ -0,0 +1,9 @@
+package torvalds.istinventorymanagement.controller;
+
+/**
+ * Created by Hassan Jegan Ndow on 2/23/2017.
+ * Handles the I/O between the Model and Main View
+ */
+
+public class MasterController {
+}
