996c1048c91f853ddd5767b1cb573a242860e47f
hjn5906 <hjn5906@rit.edu>
2017-04-15 16:22:12 -0400

    refac: changes web service url

--- android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/Constants.java
+++ android/ISTInventoryManagement/app/src/main/java/torvalds/istinventorymanagement/Constants.java
@@ -7,13 +7,13 @@ package torvalds.istinventorymanagement;
 public class Constants {
 
     // Web Service base url
-    public static final String BASE_URL = "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services2/";
+    public static final String BASE_URL = "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services/";
 
     // Mock base URL, Ivan works with this.
     public static final String MOCK_URL = "http://demo6190713.mockable.io/";
 
     // url of Jegan's local web service - testing purposes
-    public static final String JEGAN_URL = "http://10.0.2.2:8080/ISTInventoryWebService/services2/";
+    public static final String JEGAN_URL = "http://10.0.2.2:8080/ISTInventoryWebService/services/";
 
     public static final String ITEM_KEY = "ITEM_KEY";
 
