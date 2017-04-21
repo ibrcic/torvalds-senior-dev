4067b4d8601a84e0e599e0730282774cf7b8282f
Luka <lxm1660@g.rit.edu>
2017-04-21 20:51:58 +0200

    Added get single item (by id) and get single user (by id) functionality

--- angular-frontend/inventory-administration/src/public_html/js/ajaxcalls.js
+++ angular-frontend/inventory-administration/src/public_html/js/ajaxcalls.js
@@ -3,8 +3,7 @@ function getItems() {
         type: "GET",
         async: true,
         cache: false,
-        // this needs to be changed to our routes
-        url: "https://demo1622751.mockable.io/",
+        url: "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services/items/data.json",
         data: "",
         dataType: "text"
     }).fail(function() {
@@ -14,10 +13,55 @@ function getItems() {
         var result = "";
 
         for (i=0; i<json.length; ++i) {
-            result += "<a href='viewDetails.html' class='list-group-item'>" + json[i].itemTypeName + " - " + json[i].manufacturer + ", " + json[i].model + "</a>";
+            result += "<a href='viewDetails.html?id="+ json[i].idItem +"' class='list-group-item'>" + json[i].itemTypeName + " - " + json[i].manufacturer + ", " + json[i].model + "</a>";
         }
 
         $('#itemList').append(result);
+    });
+}
+
+function getItemDetails(id) {
+    $.ajax({
+        type: "GET",
+        async: true,
+        cache: false,
+        url: "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services/items/"+ id +"/data.json",
+        data: "",
+        dataType: "text"
+    }).fail(function() {
+        console.log("ajax fail");
+    }).success(function(data) {       
+        var json = jQuery.parseJSON(data);
+        var result = "";
+
+        $('#itemName').html(json.itemTypeName);
+        $('#itemModel').html(json.model);
+        $('#itemManufacturer').html(json.manufacturer);
+        $('#itemSerialNumber').html(json.serialNumber);
+        $('#itemAcquireDate').html(json.aquireDate);
+    });
+}
+
+function getUserDetails(id) {
+    $.ajax({
+        type: "GET",
+        async: true,
+        cache: false,
+        url: "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services/users/"+ id +"/data.json",
+        data: "",
+        dataType: "text"
+    }).fail(function() {
+        console.log("ajax fail");
+    }).success(function(data) {       
+        var json = jQuery.parseJSON(data);
+        var result = "";
+
+        $('#firstName').html(json.firstName);
+        $('#lastName').html(json.lastName);
+        $('#username').html(json.userName);
+        $('#email').html(json.email);
+        $('#majorTitle').html(json.majorTitle);
+        $('#className').html(json.className);
     });
 }
 
@@ -26,19 +70,17 @@ function getUsers() {
         type: "GET",
         async: true,
         cache: false,
-        // this needs to be changed to our routes
-        url: "https://demo1622751.mockable.io/",
+        url: "http://team-torvalds.ist.rit.edu:8080/LocalISTInventoryWebService/services/users/data.json",
         data: "",
         dataType: "text"
     }).fail(function() {
         console.log("ajax fail");
     }).success(function(data) {
-        console.log("Ušao u getUsers()");
         var json = jQuery.parseJSON(data);
         var result = "";
 
         for (i=0; i<json.length; ++i) {
-            result += "<a href='viewUserDetails.html' class='list-group-item'>" + json[i].userName + ", " + json[i].email + "</a>";
+            result += "<a href='viewUserDetails.html?id="+ json[i].borrowerId +"' class='list-group-item'>" + json[i].userName + ", " + json[i].email + "</a>";
         }
 
         $('#userList').append(result);
